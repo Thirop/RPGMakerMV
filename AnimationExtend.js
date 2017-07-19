eeee921a7fd079f0f2586a7a7e95a68e90fe3602
thiro-studio <thirostudio@gmail.com>
2017-07-20 00:50:20 +0900

    commit

--- AnimationExtend.js
+++ AnimationExtend.js
@@ -5,6 +5,9 @@
 // Released under the MIT license
 // http://opensource.org/licenses/mit-license.php
 //============================================================================= 
+// Version
+// 1.0.1 2017/07/20 パラメータの順序を入れ替え
+// 1.0.0 2017/07/20 初版
 
 var Imported = Imported || {};
 Imported.AnimationExtend = true;
@@ -20,53 +23,49 @@ Imported.AnimationExtend = true;
  * アニメーションIDまでのパラメータは必須で、それ以降は省略することも可能です。
  *
  * □プレイヤーにアニメーション表示
- * animation player アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation player アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
  * □実行中のイベントにアニメーション表示
- * animation this アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation this アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
  * □マップ上のイベントにアニメーション表示 
- * animation event イベントID アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation event イベントID アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
  * □戦闘中の敵にアニメーション表示
- * animation enemy 敵のインデックス アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation enemy 敵のインデックス アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
  * □戦闘中のパーティーメンバーにアニメーション表示（サイドビュー時） 
- * animation party パーティーメンバーのインデックス アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation party パーティーメンバーのインデックス アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
  * □戦闘中のアクターにアニメーション表示（サイドビュー時） 
- * animation actor アクターID アニメーションID 反転表示フラグ 遅延フレーム 拡大率(%) 角度 x軸のずれ y軸のずれ 効果音の音量
+ * animation actor アクターID アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ *
+ * 【プラグインコマンド例】
+ * animation event 1 2 50 90 20 30 true 0 40
+ *   イベントID１のイベントに
+ *   アニメーションID2のアニメーションを
+ *   拡大率５０％で
+ *   ９０度回転して
+ *   x軸に20pixel、y軸に30pixelずらして
+ *   左右反転して
+ *   表示遅延０で
+ *   効果音を40%の大きさで再生。
+ * 
+ * animation player 3 30
+ *   プレイヤーに
+ *   アニメーションID3のアニメーションを
+ *   拡大率３０%で再生
  *
  *
  * 【パラメータ解説】
  * アニメーションID : アニメーションのID
- * 反転表示フラグ : trueまたはonで左右反転表示、falseまたはoffで反転無し
- * 遅延フレーム : アニメーションを表示するまでの遅延フレーム
  * 拡大率 : アニメーションを表示する大きさ(%)。デフォルトは100
  * 角度 : アニメーションの表示角度
  * x軸のずれ : アニメーションをx軸にずらして表示するピクセル数
  * y軸のずれ : アニメーションをy軸にずらして表示するピクセル数
+ * 反転表示フラグ : trueまたはonで左右反転表示、falseまたはoffで反転無し
+ * 遅延フレーム : アニメーションを表示するまでの遅延フレーム
  * 効果音の音量 : 効果音の大きさ(%)。デフォルトは100
- *
- * 【プラグインコマンド例】
- * animation event 1 2 true 0 50 90 20 30 40
- *   イベントID１のイベントに
- *   アニメーションID2のアニメーションを
- *   左右反転して
- *   表示遅延０で
- *   拡大率５０％で
- *   ９０度回転して
- *   x軸に20pixel、y軸に30pixelずらして
- *   効果音を40%の大きさで再生。
- * 
- * 
- * animation player 3 false 0 30
- *   プレイヤーに
- *   アニメーションID3のアニメーションを
- *   表示遅延0で
- *   拡大率３０%で再生
- *
- * 
  */
 //============================================================================= 
 
@@ -123,15 +122,16 @@ Game_Interpreter.prototype.pluginCommand = function(command, args) {
 		var animationId = Number(args[idx++]);
 		if(!animationId)return;
 
-		var argMirror = args[idx++];
-		argMirror = argMirror ? argMirror.toLowerCase() : null;
-		var mirror = argMirror === 'true' || argMirror === 'on';
-		
-		var delay = supplementNum(0,args[idx++]);
 		var scale = supplementNum(100,args[idx++])/100;
 		var rotation = supplementNum(0,args[idx++])/180*Math.PI;
 		var offsetX = supplementNum(0,args[idx++]);
 		var offsetY = supplementNum(0,args[idx++]);
+
+		var argMirror = args[idx++];
+		argMirror = argMirror ? argMirror.toLowerCase() : null;
+		var mirror = argMirror === 'true' || argMirror === 'on';
+		var delay = supplementNum(0,args[idx++]);
+
 		var seVolume = supplementNum(100,args[idx++]);
 
 		if(target.requestAnimation){
