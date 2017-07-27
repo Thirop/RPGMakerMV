cfdf1ba2658ff17c8da6bc724a1e82c04e675428
thiro-studio <thirostudio@gmail.com>
2017-07-27 16:11:59 +0900

    animationExtend v1.1

--- AnimationExtend.js
+++ AnimationExtend.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 //============================================================================= 
 // Version
+// 1.1.0 2017/07/27 パラメータ記述方法の追加
 // 1.0.1 2017/07/20 パラメータの順序を入れ替え
 // 1.0.0 2017/07/20 初版
 
@@ -16,32 +17,69 @@ Imported.AnimationExtend = true;
 /*:
  * @plugindesc アニメーション表示機能を拡張
  * @author シロップ
+ *
+ * @param VE_BattleMotionsで使用
+ * @desc trueでVE_BattleMotions.jsプラグインのアクションシーケンス機能で利用
+ * Default: false
+ * @default false
+ *
+ * 
  * @help
- * 注意)アニメーション系のプラグインと競合が発生する可能性が高いです。
+ * 注)アニメーション系のプラグインと競合が発生する可能性があります。
+ * 注)V1.1でパラメータの表記方法が変わりましたが、
+ *   それ以前の方法でも指定可能です。
  * 
  * 【プラグインコマンド】
- * アニメーションIDまでのパラメータは必須で、それ以降は省略することも可能です。
+ * プラグインコマンドの書き方は、以下が基本の形です。
+ * animation ターゲット アニメーションID 各種パラメータ
  *
- * □プレイヤーにアニメーション表示
- * animation player アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ * 各種パラメータは２つの表記方法があります。
+ * （１つのコマンドで２つの方法を混ぜて使わないようにして下さい。）
+ * 
+ * □パラメータの値を順番に記述
+ * animation ターゲット アニメーションID 拡大率 角度 x y 反転 遅延 音量
+ * アニメーションID以降は省略可能です
  *
- * □実行中のイベントにアニメーション表示
- * animation this アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ * □パラメータ名:値のセットで記述
+ * animation ターゲット アニメーションID 拡大率:100 角度:0 x:10 y:20 反転:ON 遅延:30 音量:40
+ * この表記方法では、下のように好きな順番で記述もできます。
+ * animation ターゲット アニメーションID 反転:ON 音量:40 拡大率:100
  *
- * □マップ上のイベントにアニメーション表示 
- * animation event イベントID アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ * □パラメータの値に変数を使用
+ * ターゲット内に使うパラメータも含めて変数にも対応しています。
+ * 変数を使う場合は、パラメータに\V[変数ID]のように記述して下さい。
  *
- * □戦闘中の敵にアニメーション表示
- * animation enemy 敵のインデックス アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ * 【パラメータの解説】
+ * パラメータ名(代替可能なパラメータ名) : パラメータの説明
  *
- * □戦闘中のパーティーメンバーにアニメーション表示（サイドビュー時） 
- * animation party パーティーメンバーのインデックス アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
+ * アニメーションID(id) : アニメーションのID
+ * 拡大率(scale) : アニメーションを表示する大きさ(%)。デフォルトは100
+ * 角度(angle) : アニメーションの表示角度（時計回り）
+ * x : アニメーションをx軸にずらして表示するピクセル数
+ * y : アニメーションをy軸にずらして表示するピクセル数
+ * 反転(mirror) : trueまたはonで左右反転表示、falseまたはoffで反転無し。
+ *　　　　　　      mirrorだけのように値の省略も可能
+ * 遅延(delay) : アニメーションを表示するまでの遅延フレーム
+ * 音量(volume) : 効果音の大きさ(%)。デフォルトは100
  *
- * □戦闘中のアクターにアニメーション表示（サイドビュー時） 
- * animation actor アクターID アニメーションID 拡大率(%) 角度 x軸のずれ y軸のずれ 反転表示フラグ 遅延フレーム 効果音の音量
  *
- * 【プラグインコマンド例】
- * animation event 1 2 50 90 20 30 true 0 40
+ * 【ターゲットの指定】
+ * □プレイヤー => player 
+ * 「player」の代わりに「プレイヤー」も可
+ * □実行中のイベント => this
+ * □マップ上のイベント => event:イベントID
+ * 「event」の代わりに「イベント」も可
+ * □戦闘中の敵 => enemy:敵インデックス 
+ * 「enemy」の代わりに「敵」も可
+ * □戦闘中のパーティーメンバー => party:パーティーメンバーのインデックス
+ * 「party」の代わりに「パーティー」も可
+ * □戦闘中のアクターにアニメーション表示 => animation actor:アクターID
+ * 「actor」の代わりに「アクター」も可
+ *
+ *
+ * 【コマンド例】
+ * 例1)
+ * animation イベント:1 2 拡大率:50 角度:90 x:20 y:30 反転:on 遅延:0 音量:40
  *   イベントID１のイベントに
  *   アニメーションID2のアニメーションを
  *   拡大率５０％で
@@ -49,27 +87,44 @@ Imported.AnimationExtend = true;
  *   x軸に20pixel、y軸に30pixelずらして
  *   左右反転して
  *   表示遅延０で
- *   効果音を40%の大きさで再生。
+ *   効果音を40%の大きさで再生
+ *
+ * 例2)
+ * animation event:1 2 50 90 20 30 true 0 40 
+ * パラメータの表記方法を変えただけで内容は例1と同じ
+ *
+ * 例3)
+ * animation プレーヤー 1 拡大率:200 反転 角度:30
+ *   プレーヤーにアニメーションID1のアニメーションを
+ *   拡大率２００で反転して角度３０で表示。
+ *
+ * 例4)
+ * animation this 1 scale:50 ｘ:\V[2] y:\V[3]
+ * 　 このイベントに、アニメーションID１のアニメーションを
+ * 　 拡大率５０％でx座標に変数２の値、yっ座標に変数３の値ずらして表示。
+ *
+ *
+ *
+ * 【アニメーションの１コマあたりのフレーム数の設定】
+ * アニメーション名に[Fフレーム数]を戦闘につけて設定
+ * 通常のフレーム数は4で１秒あたり15コマの再生(15FPS) 
+ * 例) [F2]炎/単体1
+ * １コマあたり２フレームの持続時間で1秒に30コマ(30FPS)
+ *
+ *
+ * 【VE_BattleMotions.jsプラグインでの利用】
+ * パラメータ「VE_BattleMotions.jsプラグインでの利用」をtrueに設定
+ * 利用時はVE_BasicModuleとVE_BattleMotionsの導入必須。
+ *
+ * 通常のアニメーションコマンドに加えてパラメータを付け足して指定します
+ * (通常のコマンドの書式はVE_BattleMotionsプラグインのヘルプを参照。)
+ * animation: ターゲット, アニメーションID, 拡大率, 角度, x軸のずれ, y軸のずれ, 反転表示フラグ, 遅延フレーム, 効果音の音量
  * 
- * animation player 3 30
- *   プレイヤーに
- *   アニメーションID3のアニメーションを
- *   拡大率３０%で再生
- *
- *
- * 【パラメータ解説】
- * アニメーションID : アニメーションのID
- * 拡大率 : アニメーションを表示する大きさ(%)。デフォルトは100
- * 角度 : アニメーションの表示角度
- * x軸のずれ : アニメーションをx軸にずらして表示するピクセル数
- * y軸のずれ : アニメーションをy軸にずらして表示するピクセル数
- * 反転表示フラグ : trueまたはonで左右反転表示、falseまたはoffで反転無し
- * 遅延フレーム : アニメーションを表示するまでの遅延フレーム
- * 効果音の音量 : 効果音の大きさ(%)。デフォルトは100
  */
 //============================================================================= 
 
-
+var parameters = PluginManager.parameters('Plugin');
+var VE_BattleMotions = parameters['VE_BattleMotionsで使用'] === 'true';
 
 
 (function(){
@@ -88,6 +143,31 @@ var supplementNum = function(default_value, opt_arg, opt_callback) {
 };
 
 
+
+var convertEscapeCharacters = function(text) {
+	var idx = 0;
+    text = text.replace(/\\/g, '\x1b');
+    text = text.replace(/\x1b\x1b/g, '\\');
+    text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
+        return $gameVariables.value(parseInt(arguments[1]));
+    });
+    text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
+        return $gameVariables.value(parseInt(arguments[1]));
+    });
+    text = text.replace(/\x1bN\[(\d+)\]/gi, function() {
+    	var actor = n >= 1 ? $gameActors.actor(parseInt(arguments[1])) : null;
+	    return actor ? actor.name() : '';
+    });
+    text = text.replace(/\x1bP\[(\d+)\]/gi, function() {
+	    var actor = n >= 1 ? $gameParty.members()[parseInt(arguments[1]) - 1] : null;
+	    return actor ? actor.name() : '';
+    });
+    text = text.replace(/\x1bG/gi, TextManager.currencyUnit);
+    return text;
+};
+
+
+
 //=============================================================================
 // Game_Interpareter
 //=============================================================================
@@ -102,38 +182,84 @@ Game_Interpreter.prototype.pluginCommand = function(command, args) {
 		var target;
 		if(targetType === '0' || targetType === 'this'){
 			target = this.character();
-		}else if(targetType === 'player'){
+		}else if(['player','プレイヤー'].contains(targetType)){
 			target = $gamePlayer;
 		}else{
-		    var targetId = args[idx++];	
-		    if(targetType === 'event'){
+			var targetArgs = targetType.split(':');
+			targetType = targetArgs[0];
+		    var targetId = targetArgs[1] ? targetArgs[1] : args[idx++];
+		    targetId = Number(convertEscapeCharacters(targetId));
+
+		    if(['event','イベント'].contains(targetType)){
 		    	target = $gameMap.event(targetId);
-		    }else if(targetType === 'party'){
+		    }else if(['party','パーティー'].contains(targetType)){
 		    	target = $gameParty.members()[targetId-1];
-		    }else if(targetType === 'enemy'){
+		    }else if(['enemy','敵'].contains(targetType)){
 		    	target = $gameTroop.members()[targetId-1];
-		    }else if(targetType === 'actor'){
-		    	target = $gameActor.actor(targetId);
+		    }else if(['actor','アクター'].contains(targetType)){
+		    	target = $gameActors.actor(targetId);
 		    }
 		}
 
 		if(!target)return;
 
-		var animationId = Number(args[idx++]);
+
+		var animationId = 0;
+		var scale = 100;
+		var rotation = 0;
+		var offsetX = 0;
+		var offsetY = 0;
+		var argMirror = false;
+		var mirror = false;
+		var delay = 0;
+		var seVolume = 100;
+
+		var defaultParams = ['animationid','scale','angle','x','y','mirror','delay','sevolume'];
+
+		for(;idx<args.length; idx+=1){
+			var arg = args[idx];
+			var params = arg.split(':');
+
+			var defParam = defaultParams.shift();
+
+			var pName;
+			var pValue;
+			if(params.length === 2){
+				pName = params[0].toLowerCase();
+				pValue = params[1];
+			}else{
+				if(['mirror','反転'].contains(params[0])){
+					pName = 'mirror';
+					pValue = 'true';
+				}else{
+					pName = defParam;
+					pValue = params[0];
+				}
+			}
+
+			pValue = convertEscapeCharacters(pValue);
+
+			if(['animationid','animation','id','アニメーションID','アニメーション'].contains(pName)){
+				animationId = Number(pValue);
+			}else if(['scale','大きさ','拡大率'].contains(pName)){
+				scale = Number(pValue)/100;
+			}else if(['angle','rotation','回転','角度'].contains(pName)){
+				rotation = Number(pValue);
+				if(pName!=='rotation')rotation=rotation/180*Math.PI;
+			}else if(['x','offsetx'].contains(pName)){
+				offsetX = Number(pValue);
+			}else if(['y','offsety'].contains(pName)){
+				offsetY = Number(pValue);
+			}else if(['mirror','反転'].contains(pName)){
+				mirror = !(['false','off',0,'0',false].contains(pValue.toLowerCase()));
+			}else if(['delay','遅延'].contains(pName)){
+				delay = Number(pValue);
+			}else if(['sevolume', 'se','volume','音量']){
+				seVolume = Number(pValue);
+			}
+		}
+
 		if(!animationId)return;
-
-		var scale = supplementNum(100,args[idx++])/100;
-		var rotation = supplementNum(0,args[idx++])/180*Math.PI;
-		var offsetX = supplementNum(0,args[idx++]);
-		var offsetY = supplementNum(0,args[idx++]);
-
-		var argMirror = args[idx++];
-		argMirror = argMirror ? argMirror.toLowerCase() : null;
-		var mirror = argMirror === 'true' || argMirror === 'on';
-		var delay = supplementNum(0,args[idx++]);
-
-		var seVolume = supplementNum(100,args[idx++]);
-
 		if(target.requestAnimation){
 			target.requestAnimation(animationId, mirror, delay, scale, rotation, offsetX, offsetY, seVolume);
 		}else if(target.startAnimation){
@@ -304,5 +430,72 @@ Sprite_Animation.prototype.processTimingData = function(timing) {
 	}
 };
 
+
+//=============================================================================
+// Window_BattleLog
+//=============================================================================
+Window_BattleLog.prototype.showAnimation = function(subject, targets, animationId, mirror, delay ,scale, rotaion, offsetX, offsetY,seVolume) {
+    if (animationId < 0) {
+        this.showAttackAnimation(subject, targets);
+    } else {
+        this.showNormalAnimation(targets, animationId,mirror,delay,scale, rotaion, offsetX, offsetY,seVolume,subject);
+    }
+};
+
+Window_BattleLog.prototype.showNormalAnimation = function(targets, animationId, mirror,delay,scale, rotaion, offsetX, offsetY,seVolume,subject) {
+    var animation = $dataAnimations[animationId];
+    if (animation) {
+        delay = this.animationBaseDelay() + (delay||0);
+        var nextDelay = this.animationNextDelay();
+        targets.forEach(function(target) {
+            target.startAnimation(animationId, mirror, delay, scale, rotaion, offsetX, offsetY,seVolume, subject);
+            delay += nextDelay;
+        });
+    }
+};
+
+
+
+
+
+//=============================================================================
+// VE_BattleMotionsでの利用
+//=============================================================================
+if(VE_BattleMotions && VictorEngine){
+	Window_BattleLog.prototype.processMotionAnimation = function(motion, index, user, action, targets, target) {
+	    var list = motion.split(',');
+	    var type = list[1] ? list[1].toLowerCase().trim() : '';
+	    var subjects = this.getMotionSubjects(list[0], user, targets, target, index);
+	    for (var i = 0; i < subjects.length; i++) {
+	        var subject = subjects[i];
+	        if (subject.isSpriteVisible()) {
+	            var stack = subject === user ? index : VictorEngine.battlerIndex(subject);
+	            if (type === 'action') {
+	                var animationId = action.item().animationId;
+	            } else if (type === 'weapon') {
+	                var animationId = -1;
+	            } else {
+	                var match = type.match(/\d+/gi);
+	                var animationId = match ? Number(match[0]) : 0;
+	            }
+
+	            //以下変更
+	            if (animationId) {
+	                var item = action ? action.item() : {meta:{}};
+	                var scale = Number(list[2]) || Number(item.meta.scale) || 1;
+	                var rotation = (Number(list[3]) || Number(item.meta.rotation) || 0)/180*Math.PI;
+	                var offsetX =  Number(list[4]) || Number(item.meta.offsetX) || 0;
+	                var offsetY = Number(list[5]) || Number(item.meta.offsetY) || 0;
+	                var mirror = list[6]&&list[6].contains('true');
+	                var delay = supplementNum(0, list[7] || item.meta.delay);
+	                var seVolume = supplementNum(100,list[7] || item.meta.seVolume);
+
+	                this.showAnimation(user, [subject], animationId, mirror,delay,scale,rotation,offsetX,offsetY,seVolume);
+	            }        
+	        }
+	    }
+	    this.insert(index, 'waitForTime', 1);
+	};
+}
 
 })();
